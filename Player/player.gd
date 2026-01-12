489d4f1ab158d30e4088de173ca93e25bdf8fee8
CSbean <2261060@flourbluffschools.org>
2026-01-12 12:31:39 -0600

    added moving obstical

--- Player/player.gd
+++ Player/player.gd
@@ -4,7 +4,8 @@ class_name Player
 @export_range(1000,2000) var thrust := 1000.0
 @export_range(100.0,300) var torque_thrust := 100.0
 
-var playing := true
+
+var transitioning := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,7 +14,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if playing:
+	if !transitioning:
 		if Input.is_action_pressed("boost"):
 			apply_central_force(basis.y * delta * thrust)
 		if Input.is_action_pressed("rotate left"):
@@ -24,23 +25,25 @@ func _process(delta: float) -> void:
 		get_tree().reload_current_scene.call_deferred()
 
 func _on_body_entered(body: Node) -> void:
-	if "goal" in body.get_groups():
-		print("you win")
-		if body.file_path:
-			complete_levle(body.file_path)
-		else:
-			print("No next level found")
-	if "obsticals" in body.get_groups():
-		crash_sequence()
+	if !transitioning:
+		if "goal" in body.get_groups():
+			print("you win")
+			if body.file_path:
+				complete_levle(body.file_path)
+			else:
+				print("No next level found")
+		if "obsticals" in body.get_groups():
+			crash_sequence()
 	
 
 
 func crash_sequence()->void:
+	transitioning = true
 	print("KABOOM!")
-	playing = false
-	await get_tree().create_timer(2.5).timeout
+	await get_tree().create_timer(1).timeout
 	get_tree().reload_current_scene.call_deferred()
 
 func complete_levle(next_level_file)->void:
-	await get_tree().create_timer(2.5).timeout
+	transitioning = true
+	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file.call_deferred(next_level_file)
